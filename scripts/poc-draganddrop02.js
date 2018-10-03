47c0588997736891249fce956f7282e347dc1252
Todd Fleeman <Todd.Fleeman@coxinc.com>
2018-10-03 18:17:16 -0400

    trying some thigns

--- scripts/poc-draganddrop02.js
+++ scripts/poc-draganddrop02.js
@@ -1,4 +1,5 @@
 var cols;
+var widgetOrder = [];
 
 $(document).ready(function(){
     cols = $('#columns .column'),
@@ -46,6 +47,8 @@ function handleDrop(e){
         $(col).css({'opacity': '1.0'}).removeClass('over')
     });
 
+    saveState();
+
     return false
 }
 
@@ -54,6 +57,70 @@ function handleDragEnd(e){
     [].forEach.call(cols, function(col){
         $(col).css({'opacity': '1.0'}).removeClass('over')
     })
+}
+
+// var mutationObserver = new MutationObserver(function(mutations) {
+//     mutations.forEach(function(mutation) {
+//         var target = mutation.target;
+//         console.log($(target).attr("data-id"));
+//     });
+// });
+
+// mutationObserver.observe(node, {
+//     attributes: true,
+//     characterData: true,
+//     childList: true,
+//     subtree: true,
+//     // attributeOldValue: true,
+//     // characterDataOldValue: true,
+//     attributeFilter: ['data-id']
+// });
+
+function callback(mutationList, observer) {
+    mutationList.forEach((mutation) => {
+        switch(mutation.type) {
+        case 'childList':
+            /* One or more children have been added to and/or removed
+                from the tree; see mutation.addedNodes and
+                mutation.removedNodes */
+            break;
+        case 'attributes':
+            /* An attribute value changed on the element in
+                mutation.target; the attribute name is in
+                mutation.attributeName and its previous value is in
+                mutation.oldValue */
+            break;
+        }
+        var target = mutation.target;
+        var children = $(target).parent().children()
+        console.log(children);
+    });
+}
+
+var targetNode = document.querySelector(".container");
+var observerOptions = {
+  childList: true,
+  attributes: true,
+  subtree: true //Omit or set to false to observe only changes to the parent node.
+}
+
+var observer = new MutationObserver(callback);
+observer.observe(targetNode, observerOptions);
+
+function saveState(){
+    if (localStorage) {
+        // widgetOrder = [];
+
+        // $.each(cols, function(i, col){
+            
+        //     widgetOrder.push($(col).attr("data-id"));
+        // });
+
+        
+
+        console.log("we have local storage: ", widgetOrder);
+
+    }
 }
 
 $('#columns').on('dragstart', '.column', handleDragStart)
